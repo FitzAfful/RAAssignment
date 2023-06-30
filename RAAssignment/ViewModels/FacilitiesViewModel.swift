1f55573cdcd7d0be288c52a8708bbf33aef6a11a
Fitzgerald Afful <fitzafful@gmail.com>
2023-06-30 15:27:17 +0000

    Update FacilitiesViewModel.swift

--- RAAssignment/ViewModels/FacilitiesViewModel.swift
+++ RAAssignment/ViewModels/FacilitiesViewModel.swift
@@ -1,5 +1,40 @@
+//
+//  FacilitiesViewModel.swift
+//  RAAssignment
+//
+//  Created by Fitzgerald Afful on 29/06/2023.
+//
+
+import Foundation
+
+@MainActor class FacilitiesViewModel: ObservableObject {
+    var client = APIClient()
+    @Published var facilities: [Facility] = []
+    @Published var exclusions: [[Exclusion]] = []
     @Published var selectedOptions: [Exclusion] = []
     @Published var error: Error?
+    @Published var isFetching = false
+
+    func retrieveData() {
+        if isFetching { return }
+        isFetching = true
+
+        Task {
+            do {
+                let response = try await getFacilities()
+                facilities = response.facilities
+                exclusions = response.exclusions
+            } catch {
+                self.error = error
+            }
+            isFetching = false
+        }
+    }
+
+    func getFacilities() async throws -> FacilitiesResponse {
+        return try await client.request(for: .getFacilities)
+    }
+
     func toggleSelectedOption(option: FacilityOption, facility: Facility) {
         let selectedExclusion = Exclusion(facilityId: facility.facilityId, optionsId: option.id)
 
@@ -25,3 +60,9 @@
 
         error = nil
     }
+
+    func isSelected(option: FacilityOption, facility: Facility) -> Bool {
+        return selectedOptions.contains(where: { $0.facilityId == facility.facilityId && $0.optionsId == option.id })
+    }
+
+}
